d045e3eca1225da3551592c2d4acc7c2c76cdd3d
Mohamed Koanda <m.koanda@aol.com>
2018-08-04 19:49:05 -0700

    flask database project add

---
+++ friends_create_read/server.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request, redirect, session
+from mysqlconnection import connectToMySQL # import the function connectToMySQL from the file mysqlconnection.py
+
+app = Flask(__name__)
+app.secret_key = "monkey"
+# images = Images(app)
+
+
+@app.route('/')
+def index():
+    mysql = connectToMySQL('myfriendsdb')
+    friends = mysql = mysql.query_db('SELECT * FROM friends')
+    print('*'*50)
+    print("Fetched all friends", *friends)
+
+    return render_template('index.html', friends = friends)
+
+
+@app.route('/connect', methods=['POST'])
+def connect():
+    mysql = connectToMySQL('myfriendsdb')
+    query = 'INSERT INTO friends (first_name, last_name, occupation, created_at, updated_at) VALUES (%(firstname)s, %(lastname)s, %(occupation)s, NOW(), NOW());'
+    
+    data = {
+        'firstname': request.form['firstname'],
+        'lastname': request.form['lastname'],
+        'occupation': request.form['occupation']
+    }
+    new_friend_id = mysql.query_db(query, data)
+
+    return redirect('/')
+if __name__ == '__main__':
+    app.run(debug = True)
